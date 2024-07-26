4bd03d638b7756e8dd27815392c74fc220e5cc26
JVdeveloper2023 <jvdeveloper.2023@gmail.com>
2024-07-25 19:55:56 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PageHome from './components/PageHome'
+
+
+function App() {
+
+
+  return (
+    <>
+     <PageHome></PageHome>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PageHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageHome = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center bg-zinc-900 '>
+      <h1 className='text-6xl font-bold text-white text-center'>Proximamente <span className='text-red-400'> Floripan Oficial! </span> </h1>
+      <p className='text-xl text-white'>Por el momento nos encontramos en desarrollo!, pero muy pronto te ofreceremos lo mejor de nosotros, te sorprenderemos</p>
+    </div>
+  );
+};
+
+export default PageHome;
